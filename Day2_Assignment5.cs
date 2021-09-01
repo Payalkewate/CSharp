f482628b850350aa49f1a9f418d657562ba6eb00
Payal Kewate <89866072+Payalkewate@users.noreply.github.com>
2021-09-02 00:13:37 +0530

    Add files via upload

---
+++ Day2_Assignment5.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2_Assignment5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number of entries: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a number of Devlopers and Programming Languages");
+            List<Tuple<int, string>> list = new List<Tuple<int, string>>(n);
+            int j = 0;
+            while (j<n)
+            {
+                string line = Console.ReadLine();
+                string[] temp = line.Split(' ');
+                list.Add(new Tuple<int, string>(Convert.ToInt32(temp[0]), temp[1]));
+                j++;
+            }
+            list.Sort();
+            j = 1;
+            for(int i=n-1; i>=0; i--){
+                Console.WriteLine("Rank "+ j + " " + list[i].Item2);
+                j++;
+            }
+            Console.Read();
+        }
+    }
+}
