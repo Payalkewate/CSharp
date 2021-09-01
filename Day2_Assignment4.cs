759523cbf13837166990bb262cb9fe45bd2bc282
Payal Kewate <89866072+Payalkewate@users.noreply.github.com>
2021-09-01 23:15:15 +0530

    Add files via upload

---
+++ Day2_Assignment4.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2_Assignment4
+{
+    class Program
+    {
+        enum Days
+        {
+            Sun,
+            Mon,
+            Tue,
+            Wed,
+            Thur,
+            Fri,
+            Sat
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Number 1 to 7");
+            int day = Convert.ToInt32(Console.ReadLine());
+            day--;
+            Days myday = new Days();
+            myday = (Days)day;
+            switch (myday)
+            {
+                case Days.Mon:
+                    Console.WriteLine("Monday");
+                    break;
+                case Days.Tue:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case Days.Wed:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case Days.Thur:
+                    Console.WriteLine("Thursday");
+                    break;
+                case Days.Fri:
+                    Console.WriteLine("Friday");
+                    break;
+                case Days.Sat:
+                    Console.WriteLine("Saturday");
+                    break;
+                case Days.Sun:
+                    Console.WriteLine("Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Wrong input");
+                    break;
+
+            }
+            Console.Read();
+        }
+    }
+}
