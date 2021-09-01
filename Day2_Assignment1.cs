b04d1a3204e6521b96d3a01eb04e30340577d0e0
Payal Kewate <89866072+Payalkewate@users.noreply.github.com>
2021-09-01 22:32:54 +0530

    Add files via upload

---
+++ Day2_Assignment1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2_Assignment1
+{
+    class Program
+    {
+        //Print Output of the code  
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Round(12.5)); // output is 12
+            Console.WriteLine(Math.Round(10.5)); // output is 10
+
+            Console.Read();
+        }
+    }
+}
