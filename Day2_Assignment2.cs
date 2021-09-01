7f000dba971ba4718be72c35d30d886c988d527d
Payal Kewate <89866072+Payalkewate@users.noreply.github.com>
2021-09-01 22:33:38 +0530

    Add files via upload

---
+++ Day2_Assignment2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2_Assignment2
+{
+    class Program
+    {
+        //Print Table of the given number
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number:");
+            long n = Convert.ToInt32(Console.ReadLine());
+            for(int j=1;j<=10;j++)
+            {
+                Console.Write("{0}*{1}={2}\n", n, j, n * j);
+            }
+            Console.Read();
+        }
+    }
+}
